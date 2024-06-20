9666ebf232b16852bce28f88f8eea6b06d2196b5
Alessandro Gallo <2002alexgallo@gmail.com>
2024-06-20 16:26:42 +0200

    hearbeat on master

--- master.py
+++ master.py
@@ -1,20 +1,36 @@
 import socket
+import argparse
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/hearbeat', methods=['GET'])
+def heartbeat():
+    return jsonify({"alive": True})
+
 
 def parserInit():
     parser = argparse.ArgumentParser(
-                    prog='Slave Database 2024',
-                    epilog='by Pablo Tores Rodriguez')
-    parser.add_argument('-p ',  '--port',      type=int)
+        prog='Slave Database 2024',
+        epilog='by Pablo Tores Rodriguez')
+    parser.add_argument('-p ', '--port', type=int, default=5000)
     return parser.parse_args()
+
 
 def init():
     print('Starting master database')
     hostname = socket.gethostname()
-    IPaddr = socket.gethostbyname(hostname) 
+    IPaddr = socket.gethostbyname(hostname)
     print("Done! Master's ip is: " + IPaddr)
+    return IPaddr
+
 
 def main():
-    init()
+    args = parserInit()
+    IPaddr = init()
+    app.run(host=IPaddr, port=args.port)
+
 
 if __name__ == "__main__":
     main()
